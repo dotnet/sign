748c6a4436c9a3c77e424230e599fd40dc27e33e
Oren Novotny <oren@novotny.org>
2017-09-30 13:57:30 -0400

    Add components for UI

--- src/SignService/Controllers/HomeController.cs
+++ src/SignService/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using SignService.Models;
+using System.Diagnostics;
 
 namespace SignService.Controllers
 {
@@ -11,6 +14,12 @@ namespace SignService.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ src/SignService/Utils/AdminConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignService.Utils
+{
+    public class AdminConfig
+    {
+        public string SubscriptionId { get; set; }
+        public string ResourceGroup { get; set; }
+    }
+}
