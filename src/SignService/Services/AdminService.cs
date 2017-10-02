7f531cdf42dfecdaafb5c8935345559e1a7b0a8d
Oren Novotny <oren@novotny.org>
2017-10-01 20:43:04 -0400

    Add required fields for creating user and add random password generator

--- src/SignService/Services/AdminService.cs
+++ src/SignService/Services/AdminService.cs
@@ -8,15 +8,18 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using SignService.Models;
 using SignService.Utils;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SignService.Services
 {
     public interface IAdminService
     {
-        Task<GraphUser> CreateUserAsync(GraphUser user);
-        Task UpdateUserAsync(GraphUser user);
+        Task<(GraphUser, string)> CreateUserAsync(string displayName, string username, string keyVaultUrl, string keyVaultCertName, string timestampUrl);
+        Task UpdateUserAsync(Guid objectId, string displayName, bool? configured, string keyVaultUrl, string keyVaultCertName, string timestampUrl);
         Task<IEnumerable<GraphUser>> GetUsersAsync(string displayName);
         Task<GraphUser> GetUserByObjectIdAsync(Guid objectId);
+        Task<string> UpdatePasswordAsync(Guid objectId);
         Task<IEnumerable<GraphUser>> GetConfiguredUsersAsync();
         Task RegisterExtensionPropertiesAsync();
         Task UnRegisterExtensionPropertiesAsync();
@@ -121,18 +124,82 @@ namespace SignService.Services
         }
 
 
-        public async Task<GraphUser> CreateUserAsync(GraphUser user)
+        public async Task<(GraphUser, string)> CreateUserAsync(string displayName, string username, string keyVaultUrl, string keyVaultCertName, string timestampUrl)
         {
             var uri = $"/users?api-version=1.6";
 
+            var password = GetRandomPassword();
+
+            var user = new GraphUser
+            {
+                DisplayName = displayName,
+                UserPrincipalName = username,
+                UserType = "Guest",
+                KeyVaultUrl = keyVaultUrl,
+                KeyVaultCertificateName = keyVaultCertName,
+                TimestampUrl = timestampUrl,
+                AccountEnabled = true,
+                MailNickname = username.Substring(0, username.IndexOf('@')), // use the username up to the @
+                PasswordProfile = new PasswordProfile
+                {
+                    ForceChangePasswordNextLogin = false,
+                    Password = password
+                },
+                PasswordPolicies = "DisablePasswordExpiration, DisableStrongPassword"
+            };
+
+
             var result = await graphHttpService.Post<GraphUser, GraphUser>(uri, user).ConfigureAwait(false);
 
-            return result;
+            return (result, password);
         }
 
-        public async Task UpdateUserAsync(GraphUser user)
+        public async Task<string> UpdatePasswordAsync(Guid objectId)
         {
+            // Update the password on the account. For safety, we'll make sure this account is configured as a SignService Account first
+
+            var user = await GetUserByObjectIdAsync(objectId).ConfigureAwait(false);
             var uri = $"/users/{user.ObjectId}?api-version=1.6";
+            var password = GetRandomPassword();
+
+            if (user.SignServiceConfigured == true)
+            {
+                // new user so we don't touch existing values
+                var toUpdate = new GraphUser
+                {
+                    ObjectId = user.ObjectId,
+                    PasswordProfile = new PasswordProfile
+                    {
+                        ForceChangePasswordNextLogin = false,
+                        Password = password
+                    }
+                };
+
+                await graphHttpService.Patch(uri, user).ConfigureAwait(false);
+                return password;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can only update password of SignServiceConfigured users");
+            }
+        }
+
+        public async Task UpdateUserAsync(Guid objectId, string displayName, bool? configured, string keyVaultUrl, string keyVaultCertName, string timestampUrl)
+        {
+            
+            var uri = $"/users/{objectId}?api-version=1.6";
+
+            // Todo: this won't fully work to clear properties since null's are currently stripped on serialize
+            // Need to differentiate between default and set nulls
+            var user = new GraphUser
+            {
+                ObjectId = objectId,
+                DisplayName = string.IsNullOrWhiteSpace(displayName) ? null : displayName,
+                SignServiceConfigured = configured,
+                KeyVaultUrl = string.IsNullOrWhiteSpace(keyVaultUrl) ? null : keyVaultUrl,
+                KeyVaultCertificateName = string.IsNullOrWhiteSpace(keyVaultCertName) ? null : keyVaultCertName,
+                TimestampUrl = string.IsNullOrWhiteSpace(timestampUrl) ? null : timestampUrl
+            }; 
 
             await graphHttpService.Patch(uri, user).ConfigureAwait(false);
         }
@@ -151,6 +218,25 @@ namespace SignService.Services
             catch (Exception)
             {
                 return Enumerable.Empty<GraphUser>();
+            }
+        }
+
+        private static string GetRandomPassword()
+        {
+            // From @vcsjones, thanks!
+            const string ALLOWED_CHARS = @"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*-_!+=[]{}|\:',.?/`~""();";
+            const int PASSWORD_LENGTH = 16; // AAD has a max of 16 char passwords
+            var builder = new StringBuilder();
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var data = new byte[PASSWORD_LENGTH];
+                rng.GetBytes(data);
+                for (var i = 0; i < data.Length; i++)
+                {
+                    builder.Append(ALLOWED_CHARS[data[i] % ALLOWED_CHARS.Length]);
+                }
+
+                return builder.ToString();
             }
         }
 
