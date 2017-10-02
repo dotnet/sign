581c04b11e46f43ae011f8e9b5acdc62aaede2c7
Oren Novotny <oren@novotny.org>
2017-10-02 13:49:45 -0400

    Add Installer util and set lang version to latest

---
+++ src/InstallUtility/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace InstallUtility
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
