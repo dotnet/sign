79ae5fbbd0cc770ab36b64a4333998eaa431c958
Oren Novotny <oren@novotny.org>
2017-11-18 21:23:05 -0500

    Add client build yaml

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -1,12 +1,8 @@
-Param(
-	[string]$signClientSecret,
-	[string]$signClientUser
-)
 
 $currentDirectory = split-path $MyInvocation.MyCommand.Definition
 
 # See if we have the ClientSecret available
-if([string]::IsNullOrEmpty($signClientSecret)){
+if([string]::IsNullOrEmpty($Env:SignClientSecret)){
 	Write-Host "Client Secret not found, not signing packages"
 	return;
 }
@@ -18,12 +14,12 @@ $fileList = "$currentDirectory\filelist.txt"
 
 $appPath = "$currentDirectory\..\src\SignClient\bin\Release\netcoreapp2.0\publish\SignClient.dll"
 
-$nupgks = ls $currentDirectory\..\*.nupkg | Select -ExpandProperty FullName
+$nupgks = ls $Env:ArtifactDirectory\*.nupkg | Select -ExpandProperty FullName
 
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	dotnet $appPath 'sign' -c $appSettings -i $nupkg -f $fileList -r $signClientUser -s $signClientSecret -n 'SignClient' -d 'SignClient' -u 'https://github.com/onovotny/SignService' 
+	dotnet $appPath 'sign' -c $appSettings -i $nupkg -f $fileList -r $Env:SignClientUser -s $Env:SignClientSecret -n 'SignClient' -d 'SignClient' -u 'https://github.com/onovotny/SignService' 
 
 	Write-Host "Finished signing $nupkg"
 }
