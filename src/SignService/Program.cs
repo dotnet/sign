86a6a8e1c670ae9fa696d5b58294f60373296400
Oren Novotny <oren@novotny.org>
2017-12-08 16:51:44 -0500

    Explicitly add AI

--- src/SignService/Program.cs
+++ src/SignService/Program.cs
@@ -36,6 +36,7 @@ namespace SignService
                                                     }
                                                 }))
                    .UseStartup<Startup>()
+                   .UseApplicationInsights()
                    .Build();
     }
 }
