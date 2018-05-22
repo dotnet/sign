6bbb9fcd96d48248086accdec3181efe8a425db5
Oren Novotny <oren@novotny.org>
2018-05-22 17:03:58 -0400

    install tool locally

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -12,7 +12,7 @@ if([string]::IsNullOrEmpty($Env:SignClientSecret)){
 $appSettings = "$currentDirectory\appsettings.json"
 $fileList = "$currentDirectory\filelist.txt"
 
-dotnet tool install -g --version $Env:NBGV_SemVer2 --source-feed $Env:ArtifactDirectory SignClient 
+dotnet tool install --tool-path "$currentDirectory" --version $Env:NBGV_SemVer2 --source-feed $Env:ArtifactDirectory SignClient 
 
 # VSTS workaround for now
 $env:PATH = "$env:PATH;$env:USERPROFILE/.dotnet/tools"
@@ -22,7 +22,7 @@ $nupgks = ls $Env:ArtifactDirectory\*.nupkg | Select -ExpandProperty FullName
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	SignClient 'sign' -c $appSettings -i $nupkg -f $fileList -r $Env:SignClientUser -s $Env:SignClientSecret -n 'SignClient' -d 'SignClient' -u 'https://github.com/onovotny/SignService' 
+	& "$currentDirectory\SignClient" 'sign' -c $appSettings -i $nupkg -f $fileList -r $Env:SignClientUser -s $Env:SignClientSecret -n 'SignClient' -d 'SignClient' -u 'https://github.com/onovotny/SignService' 
 
 	Write-Host "Finished signing $nupkg"
 }
