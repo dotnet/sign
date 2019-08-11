307baa700614e6f6345176f7dc275e7f01fda143
Ben Adams <thundercat@illyriad.co.uk>
2019-08-11 05:21:29 +0100

    Cleaner temp file handling

--- src/SignService/Controllers/SignController.cs
+++ src/SignService/Controllers/SignController.cs
@@ -2,9 +2,10 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using SignService.SigningTools;
 using SignService.Utils;
 
@@ -16,7 +17,7 @@ namespace SignService.Controllers
     public class SignController : Controller
     {
         readonly ISigningToolAggregate codeSignAggregate;
-        readonly ILogger<SignController> logger;
+        readonly ILogger logger;
 
         public SignController(ISigningToolAggregate codeSignAggregate, ILogger<SignController> logger)
         {
@@ -28,30 +29,46 @@ namespace SignService.Controllers
         [RequestFormLimits(MultipartBodyLengthLimit = 4294967295)]
         [RequestSizeLimit(4294967295)]       
         
-        public async Task<IActionResult> SignFile(IFormFile source, IFormFile filelist, HashMode hashMode, string name, string description, string descriptionUrl)
+        public async Task SignFile(IFormFile source, IFormFile filelist, HashMode hashMode, string name, string description, string descriptionUrl)
         {
             if (source == null)
             {
-                return BadRequest();
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
             }
 
             // If we're in Key Vault enabled mode, don't allow dual since SHA-1 isn't supported
             if (hashMode == HashMode.Sha1 || hashMode == HashMode.Dual)
             {
-                ModelState.AddModelError(nameof(hashMode), "Azure Key Vault does not support SHA-1. Use sha256");
-                return BadRequest(ModelState);
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                if (Request.Protocol != "HTTP/2")
+                {
+                    // HTTP/2 no longer has reason phrases, so no point in setting it
+                    ModelState.AddModelError(nameof(hashMode), "Azure Key Vault does not support SHA-1. Use sha256");
+                    HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = ModelState.ToString();
+                }
+
+                return;
             }
 
             var dataDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
             Directory.CreateDirectory(dataDir);
-
-            // this might have two files, one containing the file list
-            // The first will be the package and the second is the filter
-
-            var inputFileName = Path.Combine(dataDir, source.FileName);
-            var sendingResponse = false;
             try
             {
+                // this might have two files, one containing the file list
+                // The first will be the package and the second is the filter
+
+                // Use a random filename rather than trusting source.FileName as it could be anything
+                var inputFileName = Path.Combine(dataDir, Path.GetRandomFileName());
+                // However check its extension as it might be important (e.g. zip, bundle, etc)
+                var ext = Path.GetExtension(source.FileName).ToLowerInvariant();
+                if (IsExtensionImportant(ext))
+                {
+                    // Keep the input extenstion as it has significance.
+                    inputFileName = Path.ChangeExtension(inputFileName, ext);
+                }
+
                 if (source.Length > 0)
                 {
                     using (var fs = new FileStream(inputFileName, FileMode.Create))
@@ -71,25 +88,57 @@ namespace SignService.Controllers
                 }
 
                 // This will block until it's done
-                await codeSignAggregate.Submit(hashMode, name, description, descriptionUrl, new[] { inputFileName }, filter);            
+                await codeSignAggregate.Submit(hashMode, name, description, descriptionUrl, new[] { inputFileName }, filter);
 
-                Response.OnCompleted(() =>
+                // Send it back with the original file name, if it had one
+                if (!string.IsNullOrEmpty(source.FileName))
                 {
-                    DirectoryUtility.SafeDelete(dataDir);
-                    return Task.CompletedTask;
-                });
+                    var contentDisposition = new ContentDispositionHeaderValue("attachment");
+                    contentDisposition.SetHttpFileName(source.FileName);
+                    Response.Headers[HeaderNames.ContentDisposition] = contentDisposition.ToString();
+                }
 
-                sendingResponse = true;
-                // Send it back with the original file name
-                return PhysicalFile(inputFileName, "application/octet-stream", source.FileName);
+                Response.ContentType = "application/octet-stream";
+
+                using (var fs = new FileStream(inputFileName, FileMode.Open))
+                {
+                    Response.StatusCode = StatusCodes.Status200OK;
+                    Response.ContentLength = fs.Length;
+                    // Output the signed file
+                    await fs.CopyToAsync(Response.Body);
+                }
             }
             finally
+            { 
+                // Clean up the temp directory
+                DirectoryUtility.SafeDelete(dataDir);
+            }
+        }
+
+        static bool IsExtensionImportant(string extension)
+        {
+            switch (extension)
             {
-                // There was some other error, make sure to clean up
-                if(!sendingResponse)
-                {
-                    DirectoryUtility.SafeDelete(dataDir);
-                }                
+                // archives
+                case ".zip":
+                case ".nupkg":
+                case ".snupkg":
+                case ".vsix":
+                case ".appxupload":
+                case ".msixupload":
+                // appxs
+                case ".appx":
+                case ".eappx":
+                case ".msix":
+                case ".emsix":
+                // bundles
+                case ".appxbundle":
+                case ".eappxbundle":
+                case ".msixbundle":
+                case ".emsixbundle":
+                    return true;
+                default:
+                    return false;
             }
         }
     }
