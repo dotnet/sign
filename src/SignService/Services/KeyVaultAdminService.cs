055c940f2de3fd412b4d28132ec1e282ac08c2da
Claire Novotny <claire@novotny.org>
2021-02-24 08:41:12 -0500

    style fixes

--- src/SignService/Services/KeyVaultAdminService.cs
+++ src/SignService/Services/KeyVaultAdminService.cs
@@ -246,7 +246,7 @@ namespace SignService.Services
 
             // for the vault name, we get up to 24 characters, so use the following:
             // upn up to the @ then a dash then fill with a guid truncated
-            var vaultName = $"{upn.Substring(0, upn.IndexOf('@'))}-{Guid.NewGuid().ToString("N")}";
+            var vaultName = $"{upn.Substring(0, upn.IndexOf('@'))}-{Guid.NewGuid():N}";
 
             // Truncate to 24 chars
             vaultName = vaultName.Substring(0, 24);
