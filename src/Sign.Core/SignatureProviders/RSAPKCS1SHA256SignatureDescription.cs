ea2007cee7be382d0c8b698d43d82c2ac8774d34
Damon Tivel <dtivel@microsoft.com>
2022-11-22 09:43:00 -0800

    Apply feedback.

--- src/Sign.Core/SignatureProviders/RSAPKCS1SHA256SignatureDescription.cs
+++ src/Sign.Core/SignatureProviders/RSAPKCS1SHA256SignatureDescription.cs
@@ -4,6 +4,7 @@ namespace Sign.Core
 {
     // This type and its default constructor are public because:
     //   "Algorithms added to CryptoConfig must be accessible from outside their assembly."
+    // See https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.cryptoconfig.addalgorithm?view=net-7.0#exceptions
     public sealed class RSAPKCS1SHA256SignatureDescription : RSAPKCS1SignatureDescription
     {
         public RSAPKCS1SHA256SignatureDescription()
