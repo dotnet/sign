4fb2cc537cd9e5862ee503393eae9bae17a457e4
Damon Tivel <dtivel@microsoft.com>
2023-04-03 10:08:30 -0700

    Set default input/output encoding to UTF8 (#619)
    Resolve https://github.com/dotnet/sign/issues/618.

--- src/Sign.Cli/Program.cs
+++ src/Sign.Cli/Program.cs
@@ -13,46 +13,49 @@ namespace Sign.Cli
     {
         internal static async Task<int> Main(string[] args)
         {
-            if (!Environment.Is64BitProcess)
+            using (new TemporaryConsoleEncoding())
             {
-                Console.Error.WriteLine(Resources.x86NotSupported);
+                if (!Environment.Is64BitProcess)
+                {
+                    Console.Error.WriteLine(Resources.x86NotSupported);
 
-                return ExitCode.Failed;
-            }
+                    return ExitCode.Failed;
+                }
 
-            AppRootDirectoryLocator locator = new();
-            DirectoryInfo appRootDirectory = locator.Directory;
+                AppRootDirectoryLocator locator = new();
+                DirectoryInfo appRootDirectory = locator.Directory;
 
-            string baseDirectory = Path.Combine(appRootDirectory.FullName, "tools", "SDK", "x64");
+                string baseDirectory = Path.Combine(appRootDirectory.FullName, "tools", "SDK", "x64");
 
-            //
-            // Ensure we invoke wintrust!DllMain before we get too far.
-            // This will call wintrust!RegisterSipsFromIniFile and read in wintrust.dll.ini
-            // to swap out some local SIPs. Internally, wintrust will call LoadLibraryW
-            // on each DLL= entry, so we need to also adjust our DLL search path or we'll
-            // load unwanted system-provided copies.
-            //
-            Kernel32.SetDllDirectoryW(baseDirectory);
-            Kernel32.LoadLibraryW($@"{baseDirectory}\wintrust.dll");
-            Kernel32.LoadLibraryW($@"{baseDirectory}\mssign32.dll");
+                //
+                // Ensure we invoke wintrust!DllMain before we get too far.
+                // This will call wintrust!RegisterSipsFromIniFile and read in wintrust.dll.ini
+                // to swap out some local SIPs. Internally, wintrust will call LoadLibraryW
+                // on each DLL= entry, so we need to also adjust our DLL search path or we'll
+                // load unwanted system-provided copies.
+                //
+                Kernel32.SetDllDirectoryW(baseDirectory);
+                Kernel32.LoadLibraryW($@"{baseDirectory}\wintrust.dll");
+                Kernel32.LoadLibraryW($@"{baseDirectory}\mssign32.dll");
 
-            // This is here because we need to P/Invoke into clr.dll for _AxlPublicKeyBlobToPublicKeyToken.
-            string windir = Environment.GetEnvironmentVariable("windir")!;
-            string netfxDir = $@"{windir}\Microsoft.NET\Framework64\v4.0.30319";
+                // This is here because we need to P/Invoke into clr.dll for _AxlPublicKeyBlobToPublicKeyToken.
+                string windir = Environment.GetEnvironmentVariable("windir")!;
+                string netfxDir = $@"{windir}\Microsoft.NET\Framework64\v4.0.30319";
 
-            AddEnvironmentPath(netfxDir);
+                AddEnvironmentPath(netfxDir);
 
-            try
-            {
-                Parser parser = CreateParser();
+                try
+                {
+                    Parser parser = CreateParser();
 
-                return await parser.InvokeAsync(args);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                    return await parser.InvokeAsync(args);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
 
-                return ExitCode.Failed;
+                    return ExitCode.Failed;
+                }
             }
         }
 
