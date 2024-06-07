817f081e94032ec65ef585c764d4ba40670b2af6
Javier <javier.delagarza@gmail.com>
2024-06-07 15:21:16 -0500

    Apply suggestions from code review
    Co-authored-by: Damon Tivel <dtivel@microsoft.com>

--- src/Sign.Cli/Helpers/HashAlgorithmParser.cs
+++ src/Sign.Cli/Helpers/HashAlgorithmParser.cs
@@ -2,15 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE.txt file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
 using System.CommandLine.Parsing;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Globalization;
-using System.Text;
-using System.Threading.Tasks;
-using System.CommandLine;
+using System.Security.Cryptography;
 
 namespace Sign.Cli
 {
