11499e034ed9e2dd912568e5a5b62919d68d0005
Jack Murray <jack.murray@trayport.com>
2024-08-28 10:08:36 +0100

    fix file path and version string

--- test/Sign.Core.Test/SignatureProviders/ClickOnceSignerTests.cs
+++ test/Sign.Core.Test/SignatureProviders/ClickOnceSignerTests.cs
@@ -16,15 +16,15 @@ namespace Sign.Core.Test
 
         private const string DeploymentManifestValidContent = @"<?xml version=""1.0"" encoding=""utf-8""?>
             <asmv1:assembly xsi:schemaLocation=""urn:schemas-microsoft-com:asm.v1 assembly.adaptive.xsd"" manifestVersion=""1.0"" xmlns:asmv1=""urn:schemas-microsoft-com:asm.v1"" xmlns=""urn:schemas-microsoft-com:asm.v2"" xmlns:asmv2=""urn:schemas-microsoft-com:asm.v2"" xmlns:xrml=""urn:mpeg:mpeg21:2003:01-REL-R-NS"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:asmv3=""urn:schemas-microsoft-com:asm.v3"" xmlns:dsig=""http://www.w3.org/2000/09/xmldsig#"" xmlns:co.v1=""urn:schemas-microsoft-com:clickonce.v1"" xmlns:co.v2=""urn:schemas-microsoft-com:clickonce.v2"">
-                <assemblyIdentity name=""MyApp.application"" version=""0.0.1.15"" publicKeyToken=""0000000000000000"" language=""neutral"" processorArchitecture=""msil"" xmlns=""urn:schemas-microsoft-com:asm.v1"" />
+                <assemblyIdentity name=""MyApp.application"" version=""1.0.0.0"" publicKeyToken=""0000000000000000"" language=""neutral"" processorArchitecture=""msil"" xmlns=""urn:schemas-microsoft-com:asm.v1"" />
                 <description asmv2:publisher=""Contoso Limited"" asmv2:product=""MyApp"" xmlns=""urn:schemas-microsoft-com:asm.v1"" />
                 <deployment install=""false"" />
                 <compatibleFrameworks xmlns=""urn:schemas-microsoft-com:clickonce.v2"">
                     <framework targetVersion=""4.8"" profile=""Full"" supportedRuntime=""4.0.30319"" />
                 </compatibleFrameworks>
                 <dependency>
-                    <dependentAssembly dependencyType=""install"" codebase=""MyApp.dll.manifest"" size=""14853"">
-                        <assemblyIdentity name=""MyApp.dll"" version=""0.0.1.15"" publicKeyToken=""0000000000000000"" language=""neutral"" processorArchitecture=""msil"" type=""win32"" />
+                    <dependentAssembly dependencyType=""install"" codebase=""MyApp_1_0_0_0\MyApp.dll.manifest"" size=""14853"">
+                        <assemblyIdentity name=""MyApp.dll"" version=""1.0.0.0"" publicKeyToken=""0000000000000000"" language=""neutral"" processorArchitecture=""msil"" type=""win32"" />
                     </dependentAssembly>
                 </dependency>
             </asmv1:assembly>
