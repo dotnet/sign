5b02eb918faa14d7cc7f9dbefcefef75289b798d
Claire Novotny <claire@novotny.org>
2024-12-13 14:12:20 -0500

    Handle the error with a better output message instead of a stack trace when signing fails

---
+++ src/Sign.Core/SigningException.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+namespace Sign.Core
+{
+    internal class SigningException : Exception
+    {
+        public SigningException()
+        {
+        }
+        public SigningException(string message)
+            : base(message)
+        {
+        }
+        public SigningException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
