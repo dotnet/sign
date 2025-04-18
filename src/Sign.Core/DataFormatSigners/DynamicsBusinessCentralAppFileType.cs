1ddbd2ae338e1209f8c41d41abbb8b6d87b2b3e2
Damon Tivel <dtivel@microsoft.com>
2025-04-18 12:00:54 -0700

    Improve detection of Dynamics Business Central `.app` files (#846)
    Resolve https://github.com/dotnet/sign/issues/824.

---
+++ src/Sign.Core/DataFormatSigners/DynamicsBusinessCentralAppFileType.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+namespace Sign.Core
+{
+    internal sealed class DynamicsBusinessCentralAppFileType : ISignableFileType
+    {
+        private const string FileExtension = ".app";
+
+        private readonly byte[] _expectedHeader;
+
+        internal DynamicsBusinessCentralAppFileType()
+        {
+            _expectedHeader = new byte[] { 0x4e, 0x41, 0x56, 0x58 }; // NAVX
+        }
+
+        public bool IsMatch(FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file, nameof(file));
+
+            if (!FileExtension.Equals(file.Extension, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            using (FileStream stream = file.OpenRead())
+            {
+                var header = new byte[_expectedHeader.Length];
+
+                if (stream.Read(header, offset: 0, header.Length) != header.Length)
+                {
+                    return false;
+                }
+
+                return header.SequenceEqual(_expectedHeader);
+            }
+        }
+    }
+}
---
+++ src/Sign.Core/DataFormatSigners/SignableFileTypeByExtension.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+namespace Sign.Core
+{
+    internal sealed class SignableFileTypeByExtension : ISignableFileType
+    {
+        private readonly HashSet<string> _fileExtensions;
+
+        internal SignableFileTypeByExtension(params string[] fileExtensions)
+        {
+            ArgumentNullException.ThrowIfNull(fileExtensions, nameof(fileExtensions));
+
+            if (fileExtensions.Length == 0)
+            {
+                throw new ArgumentException(Resources.ArgumentCannotBeEmpty, nameof(fileExtensions));
+            }
+
+            _fileExtensions = new HashSet<string>(fileExtensions, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public bool IsMatch(FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file, nameof(file));
+
+            return _fileExtensions.Contains(file.Extension);
+        }
+    }
+}
