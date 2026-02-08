1c36bee4d669fe611dc4bef76ddf11ea46a98a28
Damon Tivel <dtivel@microsoft.com>
2026-02-08 10:32:10 -0800

    Add ClickOnce interface abstractions and build configuration
    Introduces all core abstractions for ClickOnce signing:
    - IClickOnceApp: Main application abstraction
    - IClickOnceAppFactory: Factory for creating app instances
    - IApplicationManifest: Application manifest abstraction
    - IDeployManifest: Deployment manifest abstraction
    
    Also updates build configuration for new dependencies:
    - Directory.Build.props: Updated build properties
    - Directory.Packages.props: Updated package versions
    - NuGetMSBuildFix.props: NuGet MSBuild configuration
    - Sign.Core.csproj: Added Microsoft.Build.Tasks.Core reference
    
    These interfaces establish contracts that subsequent implementations will fulfill.
    
    Part of #681

---
+++ src/Sign.Core/Tools/ClickOnce/IDeployManifest.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+using Microsoft.Build.Tasks.Deployment.ManifestUtilities;
+
+namespace Sign.Core
+{
+    internal interface IDeployManifest
+    {
+        AssemblyReference? EntryPoint { get; }
+        bool MapFileExtensions { get; }
+        OutputMessageCollection OutputMessages { get; }
+        bool ReadOnly { get; set; }
+
+        void ResolveFiles(string[] searchPaths);
+        void UpdateFileInfo();
+    }
+}
